0c03402aaf6c956bd24950aba7a6e921e76fc85c
Vivek Aswal <60192479+Vivek-23-Titan@users.noreply.github.com>
2020-06-25 16:25:04 +0530

    Update detect.py

--- detect.py
+++ detect.py
@@ -22,7 +22,7 @@ parser.add_argument('--confidence_threshold', default=0.02, type=float, help='co
 parser.add_argument('--top_k', default=5000, type=int, help='top_k')
 parser.add_argument('--nms_threshold', default=0.4, type=float, help='nms_threshold')
 parser.add_argument('--keep_top_k', default=750, type=int, help='keep_top_k')
-parser.add_argument('-s', '--save_image', action="store_true", default=True, help='show detection results')
+parser.add_argument('-s', '--save_image', help='show detection results')
 parser.add_argument('--vis_thres', default=0.6, type=float, help='visualization_threshold')
 args = parser.parse_args()
 
@@ -84,7 +84,7 @@ if __name__ == '__main__':
 
     # testing begin
     for i in range(100):
-        image_path = "./curve/test.jpg"
+        image_path = "/content/frames/*.jpg"
         img_raw = cv2.imread(image_path, cv2.IMREAD_COLOR)
 
         img = np.float32(img_raw)
